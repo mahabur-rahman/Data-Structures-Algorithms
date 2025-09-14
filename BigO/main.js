c6056a1d4ac68a32294ca2127bdbca72e63ad118
mahabur-rahman <annur4395@gmail.com>
2025-09-14 23:59:51 +0600

    feat: integration time complexity with memory or space management with o(n)

--- BigO/main.js
+++ BigO/main.js
@@ -1,13 +1,23 @@
-const nemo = ["nemo"];
+// const nemo = ["nemo"];
 
-function findingNemo(nemo) {
-  for (let i = 0; i < nemo.length; i++) {
-    if (nemo[i] === "nemo") {
-      console.log(`finding : ${nemo[i]}`);
-    } else {
-      console.log(`Nomo not found!`);
-    }
-  }
+// function findingNemo(nemo) {
+//   for (let i = 0; i < nemo.length; i++) {
+//     if (nemo[i] === "nemo") {
+//       console.log(`finding : ${nemo[i]}`);
+//     } else {
+//       console.log(`Nomo not found!`);
+//     }
+//   }
+// }
+
+// findingNemo(nemo);
+
+
+
+function getFirstElement(arr) {
+  console.log(`array: `, arr[0])
+    return arr[0]
 }
 
-findingNemo(nemo);
+
+getFirstElement([1,2,3,4,5]) // 0(1) constant time complexity
