ec059a6a7c1d265c77bf1071862106e0836a5eb7
mahabur-rahman <annur4395@gmail.com>
2025-09-30 17:18:28 +0600

    feat: max or min number finding solution

---
+++ Array_String/index.js
@@ -0,0 +1,32 @@
+// const arr = [34, 4, 5, 6, 6, 55, 6, 7, 432, 1];
+
+// function sumOfArray(arr) {
+//   let result = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     result += arr[i]; // result + arr[i]
+//   }
+
+//   return result;
+// }
+
+// console.log(sumOfArray(arr));
+
+
+
+// function findMax(arr) {
+//   if (arr.length === 0) {
+//     return "Array is empty!";
+//   }
+
+//   let max = arr[0];
+
+//   for (let i = 1; i < arr.length; i++) {
+//     if (max > arr[i]) {
+//       max = arr[i];
+//     }
+//   }
+//   return max;
+// }
+
+// const numbers = [3000, 8, 1, 9, 4, 67, 2, 900];
+// console.log(findMax(numbers));
